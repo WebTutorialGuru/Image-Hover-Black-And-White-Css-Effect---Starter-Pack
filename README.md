99baba26eb31f82a29fc399c482a1b4a7edadd3e
WebTutorialGuru <WebTutorialGuru@gmail.com>
2020-08-15 13:33:11 +0530

    #1

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Image-Hover-Black-And-White-Css-Effect---Starter-Pack
+
+# FREE FREE FREE Click the link below for more source code like this
+
+# http://bitly.com/WTG_sub
+
+# Check our website for more free source code
+
+# http://webtutorialguru.com
---
+++ script.js
@@ -0,0 +1,19 @@
+var style = [
+    "background: linear-gradient(#D33106, #571402)",
+    "border: 1px solid #3E0E02",
+    "color: white",
+    "display: block",
+    "text-shadow: 0 1px 0 rgba(0, 0, 0, 0.3)",
+    "line-height: 40px",
+    "font-size: 25px",
+    "text-align: center",
+    "text-align: center",
+    "font-weight: bold",
+].join(";");
+
+console.log(
+    "%c FREE FREE FREE Click the link below for more source code like this ",
+    style
+);
+console.log("%c http://bitly.com/WTG_sub ", style);
+console.log("%c http://webtutorialguru.com ", style);
